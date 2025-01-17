261efb82bf2f8a712fc26e220d7190237240840a
devDaman <damandeep611@gmail.com>
2025-01-17 13:22:54 +0530

    render error fixing 1

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://apollotracker-backend.onrender.com"
+    ? "https://apollotracker-backend.onrender.com/todos"
     : "http://localhost:5000/api";
 
 export default API_BASE_URL;
